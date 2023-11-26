cc01b80a2175f2214352c40fdb71e615fcff46bf
robertoquintana <2im3.quintana.esc.azul@gmail.com>
2023-11-26 03:00:34 -0600

    se modifico sensores_BD.php\n conexion a la BD creada exitosamente, nota la tabla reportes ya no cuenta con el campo automatico

--- ServerThinkSpeak/sensores_BD.php
+++ ServerThinkSpeak/sensores_BD.php
@@ -1,15 +1,37 @@
 <?php
-    // Comprobar el valor del POST
-    $temperatura = $_POST['temperatura'] ?? null;
-    $iluminacion = $_POST['iluminacion'] ?? null;
-    
-    // if ($temperatura !== null) {
-    //     if ($temperatura > 50) {
-    //         echo "1"; // Si la temperatura es mayor que 50, enviar "1"
-    //     } else {
-    //         echo "0"; // Si la temperatura es menor o igual a 50, enviar "0"
-    //     }
-    // }
+// Obtener los valores del POST
+$temperatura = $_POST['temperatura'] ?? null;
+$iluminacion = $_POST['iluminacion'] ?? null;
 
-    echo "temperatura=" . $temperatura . "&iluminacion=" . $iluminacion;
+// Obtener la fecha y hora actual
+$fecha = date('Y-m-d');
+$hora = date('H:i:s');
+
+// Credenciales de la base de datos
+$host = "localhost";
+$db = "id21371168_invernadero";
+$user = "id21371168_root";
+$password = "W3b0s1.0";
+
+// Intentar establecer la conexión
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Aquí puedes realizar operaciones con la base de datos si la conexión fue exitosa.
+    // Por ejemplo, podrías insertar los valores de temperatura, iluminación, fecha y hora en una tabla.
+
+    // Ejemplo de inserción de datos en una tabla llamada 'reporte'
+    $stmt = $conn->prepare("INSERT INTO reporte (temperatura, iluminacion, fecha, hora) VALUES (:temperatura, :iluminacion, :fecha, :hora)");
+    $stmt->bindParam(':temperatura', $temperatura);
+    $stmt->bindParam(':iluminacion', $iluminacion);
+    $stmt->bindParam(':fecha', $fecha);
+    $stmt->bindParam(':hora', $hora);
+    $stmt->execute();
+
+    echo "Datos insertados correctamente en la base de datos.";
+
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
 ?>
