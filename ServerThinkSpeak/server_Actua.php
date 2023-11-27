f278cdbc39cd3ec51e12e2fc6f5a881fa84d9d92
robertoquintana <2im3.quintana.esc.azul@gmail.com>
2023-11-27 00:28:42 -0600

    Ahora server_Actua.php funciona obtiene los datos de ambas tablas de la bd para pasarlos al node

--- ServerThinkSpeak/server_Actua.php
+++ ServerThinkSpeak/server_Actua.php
@@ -1,17 +1,5 @@
 <?php
-// Paso 1: Obtener del formulario el modo ("Automatico" o "Manual")
-$modo = $_POST['Modo'];
-
-// Paso 2: Obtener el estado de la lámpara y el ventilador según el modo
-if ($modo == "Manual") {
-    $estadoLampara =  $_POST['lampara_estado'];
-    $estadoVentilador = $_POST['ventilador_estado'];
-} elseif ($modo == "Automatico") {
-    $estadoLampara = "Automatico";
-    $estadoVentilador = "Automatico";
-}
-
-// Paso 3: Realizar la conexión con la base de datos "reporte"
+// Paso 2: Realizar la conexión con la base de datos "reporte"
 $host = "localhost";
 $db = "id21371168_invernadero";
 $user = "id21371168_root";
@@ -23,6 +11,20 @@ try {
     echo "Error de conexión: " . $e->getMessage();
     die();
 }
+
+// Paso 2: Obtener el estado de la lámpara y el ventilador según el modo
+$query = "SELECT lampara, ventilador FROM estadoActuadores ORDER BY id_estado DESC LIMIT 1";
+$result = $pdo->query($query);
+
+if ($result) {
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+    $estadoLampara = $row['lampara'];
+    $estadoVentilador = $row['ventilador'];
+} else {
+    $estadoLampara = "No disponible";
+    $estadoVentilador = "No disponible";
+}
+
 
 // Paso 4: Generar una consulta que obtenga los datos de temperatura e iluminación del último registro insertado
 $query = "SELECT temperatura, iluminacion FROM reporte ORDER BY id_reporte DESC LIMIT 1";
